0caee34d23e82a3a65a75225dbdeb7da6b0e2ec7
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2024-09-04 20:51:26 +0700

    feat: 🎸 add database migration

--- app/database.go
+++ app/database.go
@@ -8,7 +8,7 @@ import (
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3333)/belajar_golang_restfull_api")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3333)/belajar_golang_database_migration")
 	helper.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
@@ -17,4 +17,13 @@ func NewDB() *sql.DB {
 	db.SetConnMaxIdleTime(10 * time.Minute)
 
 	return db
+
+	// migrate create -ext sql -dir db/migrations create_table_first
+	// migrate create -ext sql -dir db/migrations create_table_second
+	// migrate create -ext sql -dir db/migrations create_table_third
+	// migrate create -ext sql -dir db/migrations sample_dirty_state
+	// migrate -database "mysql://root:root@tcp(localhost:3333)/belajar_golang_database_migration" -path db/migrations up
+	// migrate -database "mysql://root:root@tcp(localhost:3333)/belajar_golang_database_migration" -path db/migrations down
+	// migrate -database "mysql://root:root@tcp(localhost:3333)/belajar_golang_database_migration" -path db/migrations version
+	// migrate -database "mysql://root@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations force 20240904024200 // This is a version before Dirty occurs to be able to improve the failed migrate
 }
